21ec15597085061cbd03e06087e46a5063590d25
ShabntanuBhoge <bhogeshantanu2005@gmail.com>
2026-08-04 08:55:27 +0530

    double border
    new single border

---
+++ DiagonalWithSingleBorder3.java
@@ -0,0 +1,25 @@
+class DiagonalWithSingleBorder3
+{
+	public static void main(String[] args) 
+	{
+		int n =new java.util.Scanner(System.in).nextInt();
+		
+        for(int i=1;i<=n;i++)
+		{
+			for(int j =1;j<=n;j++)
+			{
+				if(i<=2 || j==1 || i>=n-1 || j==n || i==j)
+				{
+					System.out.print("* ");
+				}
+				else
+				{
+					System.out.print(" "+" ");
+				}
+				
+			}
+			System.out.println();
+		}
+         
+	}
+}
---
+++ DiagonalWithSingleBorder4.java
@@ -0,0 +1,25 @@
+class DiagonalWithSingleBorder4
+{
+	public static void main(String[] args) 
+	{
+		int n =new java.util.Scanner(System.in).nextInt();
+		
+        for(int i=1;i<=n;i++)
+		{
+			for(int j =1;j<=n;j++)
+			{
+				if(i==1 || j<=2 || i==n || j>=n-1 || i+j==n+1)
+				{
+					System.out.print("* ");
+				}
+				else
+				{
+					System.out.print(" "+" ");
+				}
+				
+			}
+			System.out.println();
+		}
+         
+	}
+}
